4612a755eeb546461a545174f7eb775d32d1606e
zahin <zahin.dev@gmail.com>
2019-01-30 22:09:56 -0500

    planner logic implemented

--- app/endpoints/Planner.py
+++ app/endpoints/Planner.py
@@ -15,7 +15,7 @@ class PlannerResource(Resource):
         # coursesInput['course'] returns a list of all courses input
         futureCourses = PlannerLogic(coursesInput['course'])
         return {
-            'Access-Control-Allow-Origin', '*'
+            'Access-Control-Allow-Origin':'*'
             'status': 'success',
             'data': futureCourses
         }, 200
