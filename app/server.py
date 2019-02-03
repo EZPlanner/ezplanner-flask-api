fa7c87d923907f71ef083c8c908ed280417a18aa
Atul Bipin <atulbipin@gmail.com>
2019-02-02 22:02:23 -0800

    Change folder name

--- app/server.py
+++ app/server.py
@@ -1,9 +1,9 @@
 from flask import Blueprint
 from flask_restful import Api
-from app.endpoints.Courses import CoursesResource
-from app.endpoints.PreReq import PreReqResource
-from app.endpoints.PostReq import PostReqResource
-from app.endpoints.Planner import PlannerResource
+from app.resources.Courses import CoursesResource
+from app.resources.PreReq import PreReqResource
+from app.resources.PostReq import PostReqResource
+from app.resources.Planner import PlannerResource
 
 api_bp = Blueprint('api', __name__)
 api = Api(api_bp)
