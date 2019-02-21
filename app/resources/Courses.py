bec86f85c3137d8569bc95b0a5acd573033e25cb
Atul Bipin <atulbipin@gmail.com>
2019-02-20 18:54:22 -0800

    Get rid of redundant comments

--- app/resources/Courses.py
+++ app/resources/Courses.py
@@ -9,18 +9,11 @@ course_schema = CourseSchema()
 class CoursesResource(Resource):
     def get(self):
         courses = {}
-        # try:
+
         courses = Course.query.all()
         courses = courses_schema.dump(courses).data
-        # except:
-        #     with open('./app/JSON/courses.json') as f:
-        #         courses = json.load(f)
+
         return {
             'status': 'success',
             'data': courses
         }, 200
-
-    def post(self):
-        return {
-            'message': 'Post mesages not accepted!'
-        }, 400
